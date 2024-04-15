e5755828c8bd6fd93108cbadfb67cb281c35ae72
omidrm99 <omidrm99@gmail.com>
2024-04-15 13:20:15 +0330

    index file updated

--- src/public/index.php
+++ src/public/index.php
@@ -4,14 +4,23 @@ declare(strict_types=1);
 require __DIR__ . '/../../vendor/autoload.php';
 
 use App\pagination\PaginatorView;
+use App\request\CommandReader;
+use App\validator\CommandValidation;
 const csv_File_Path = __DIR__ . '/../database/books.csv';
 const json_File_Path = __DIR__ . '/../database/books.json';
+$commandReader = new CommandReader();
+$commandValidation = new CommandValidation();
 
-try {
-    $perPage = isset($_GET['per_page']) ? $_GET['per_page'] : 5;
-    $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
-    $paginatorView = new PaginatorView($perPage, $page);
-} catch (Exception $e) {
-    echo "Error: " . $e->getMessage();
+if ($commandValidation->checkCommand($commandReader->getCommand())){
+    try {
+        $perPage = isset($_GET['per_page']) ? $_GET['per_page'] : 5;
+        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+        $paginatorView = new PaginatorView($perPage, $page);
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}else {
+    echo 'wrong command PREPARE TO DIE ';
 }
+
 
